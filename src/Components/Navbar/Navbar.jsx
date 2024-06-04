b55b726788533a1e69e5506f9090d143ee589856
Anthony Silva <anthony.silvac@gmail.com>
2024-06-04 19:44:56 -0400

    menu responsive ok

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,23 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import logo from '../../assets/logo.png'
 import './Navbar.css'
 import { menuItems } from '../../Utils/menuItems'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     const location = useLocation();
+    const [menuOpen, setMenuOpen] = useState (false)
+
+
+
       return (
    <nav>
     <div className='logo'>
-        <img src={logo} alt="logo" />
+    <Link to={"/"}><img src={logo} alt="logo" /></Link> 
     </div>
+    <div className='navMob'>
+
+    
+    <MenuIcon  fontSize="large" onClick={()=>{
+        setMenuOpen(!menuOpen)
+    }}/>
+    </div>
+    
+    
     <div className='navbar'>
+        <ul className={menuOpen ? "open" : ""}>
+          
         {menuItems.map((item, index)=>(
-            <Link key={index} to={item.route} >
+
+                <li>
+                <Link key={index} to={item.route} >
                 <h5 className={`menu-item ${location.pathname === item.route ? 'active' : ''}`}> {item.text} </h5>
-            </Link>
-        ))}
+                </Link>
+                </li>
+              
+              
+            ))}
+            
+            </ul>
     </div>
+    
+
+    
+    
+    
    </nav>
   )
 }
--- src/Utils/menuItems.js
+++ src/Utils/menuItems.js
@@ -1,7 +1,6 @@
 import { routes } from '../Utils/routes'
 
 export const menuItems = [
-    { text: 'Inicio', route: routes.home },
     { text: 'Proyectos', route: routes.project },
     { text: 'Sobre mí', route: routes.about },
     { text: 'Educación', route: routes.education },
